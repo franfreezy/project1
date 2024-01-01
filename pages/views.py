f21b687931370e47a8de0606da306173ced403ee
franfreezy <frandelwanjawa@yahoo.com>
2024-01-01 14:07:21 +0300

    additional

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse //we are not using http again since are using templates
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse('hello world')
+    return render(request, "blog/blog.html")
