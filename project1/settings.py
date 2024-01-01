5546ae0f6e045cf418bf5728b82e62681dbfe6f1
franfreezy <frandelwanjawa@yahoo.com>
2024-01-01 16:03:43 +0300

    Few tweaks here and there

--- project1/settings.py
+++ project1/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/pages/static/'
+STATICFILES_DIRS = [BASE_DIR / 'pages/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
